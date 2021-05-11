c03f606d6f53df7c44f1d4641f279339223f74b1
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-05-10 22:42:50 -0600

    created the products component with its styles, created the data.json file to store information about the menu and mapped through it inside the products component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Hero from "./components/Hero/Hero";
+import { productData } from "./components/Products/data";
+import Products from "./components/Products/Products";
 
 import { GlobalStyles } from "./globalStyles.styles";
 
@@ -10,6 +12,7 @@ function App() {
     <Router>
       <GlobalStyles />
       <Hero />
+      <Products heading="Choose your favorite" data={productData} />
     </Router>
   );
 }
---
+++ src/components/Products/Products.styles.jsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+const ProductsContainer = styled.div`
+  /* width: 100vw; */
+  min-height: 100vh;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  background: #150f0f;
+  color: #fff;
+`;
+
+const ProductWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
+`;
+
+const ProductCard = styled.div`
+  margin: 0 2rem;
+  line-height: 2;
+  width: 300px;
+`;
+
+const ProductImg = styled.img`
+  height: 300px;
+  min-width: 300px;
+  max-width: 100%;
+  box-shadow: 8px 8px #fdc500;
+`;
+
+const ProductsHeading = styled.h1`
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+`;
+
+const ProductTitle = styled.h2`
+  font-weight: 400;
+  font-size: 1.5rem;
+`;
+
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  text-align: center;
+`;
+
+const ProductDesc = styled.p`
+  margin-bottom: 1rem;
+`;
+
+const ProductPrice = styled.p`
+  margin-bottom: 1rem;
+  font-size: 2rem;
+`;
+
+const ProductButton = styled.button`
+  font-size: 1rem;
+  padding: 1rem 4rem;
+  border: none;
+  background: #e31837;
+  color: #fff;
+  transition: 0.2 ease-out;
+  &:hover {
+    background: #ffc500;
+    transition: 0.2s ease-out;
+    cursor: pointer;
+    color: #000;
+  }
+`;
+
+export {
+  ProductsContainer,
+  ProductsHeading,
+  ProductWrapper,
+  ProductCard,
+  ProductImg,
+  ProductTitle,
+  ProductDesc,
+  ProductPrice,
+  ProductButton,
+  ProductInfo,
+};
