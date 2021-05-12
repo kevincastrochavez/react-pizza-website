e231ab8416f9e3a566178a93ec26d020ac7eddcc
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-05-12 16:49:42 -0600

    added the footer omponent with its styles and finished the site

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero/Hero";
 import { productData, productDataTwo } from "./components/Products/data";
 import Products from "./components/Products/Products";
 import Feature from "./components/Feature/Feature";
+import Footer from "./components/Footer/Footer";
 
 import { GlobalStyles } from "./globalStyles.styles";
 
@@ -16,6 +17,7 @@ function App() {
       <Products heading="Choose your Favorite" data={productData} />
       <Feature />
       <Products heading="Sweet Treatsfor You" data={productDataTwo} />
+      <Footer />
     </Router>
   );
 }
